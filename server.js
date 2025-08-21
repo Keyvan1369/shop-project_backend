5ddc5fe6dec12b0c6aeea24d725d6a3be1ea09fe
Keyvan Hojabr <keyvanhojabr@gmail.com>
2025-08-21 22:16:48 +0200

    config to join frontend

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-const express = require('express')
 const mongoose = require("mongoose");
-const authRouter = require('./routes/authRouter')
-const indexRouter = require('./routes/indexRouter')
-const userRouter = require('./routes/userRouter')
-const app = express()
-const port = 5020
-app.use(express.json())
+const authRouter = require("./routes/authRouter");
+const indexRouter = require("./routes/indexRouter");
+const userRouter = require("./routes/userRouter");
+const app = express();
+const port = 5020;
 
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-
-app.use(authRouter)
-app.use(indexRouter)
-app.use(userRouter)  
+app.use("/api",authRouter);
+app.use("/api",indexRouter);
+app.use("/api",userRouter);
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to DB:", err));
 
-
-
-
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
-})
+  console.log(`app listening on port ${port}`);
+});
