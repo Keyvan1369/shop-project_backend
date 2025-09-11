ed5c3835b83f7da557874bc3e460eabe30d0dbfb
Keyvan Hojabr <keyvanhojabr@gmail.com>
2025-09-11 19:57:01 +0200

    add frontend new addresse

--- server.js
+++ server.js
@@ -2,34 +2,50 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
+
 const authRouter = require("./routes/authRouter");
 const indexRouter = require("./routes/indexRouter");
 const userRouter = require("./routes/userRouter");
 const productRouter = require("./routes/productRouter");
 const contactRouter = require("./routes/contactRouter");
+
 const app = express();
-const port = 5020;
+const port = process.env.PORT || 5020;
+
+
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://shop-project-e8tg.onrender.com", 
+  
+];
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
 
-app.use("/api",authRouter);
-app.use("/api",indexRouter);
-app.use("/api",userRouter);
-app.use("/api",productRouter);
-app.use("/api",contactRouter);
 
+app.use("/api", authRouter);
+app.use("/api", indexRouter);
+app.use("/api", userRouter);
+app.use("/api", productRouter);
+app.use("/api", contactRouter);
 
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to DB:", err));
+
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,11 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const setToken = (payload) => {
-  const token = jwt.sign(payload, "hjsdfuisdf&%$/*321045", { expiresIn: "5d" });
-
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "5d" });
   return token;
 };
 
-module.exports = {
-  setToken,
-};
+module.exports = { setToken };
